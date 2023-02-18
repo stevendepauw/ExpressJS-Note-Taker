2253cf6ea4e861a7c44c998cc4922da83bf10933
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-02-18 17:34:17 -0600

    Fixed port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes')
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
